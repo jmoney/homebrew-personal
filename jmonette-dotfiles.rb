79919725fbb3726624f1c4be68e858286b61d523
Jonathan Monette <jon.monette@gmail.com>
2013-05-05 18:32:11 -0500

    updated version

--- jmonette-dotfiles.rb
+++ jmonette-dotfiles.rb
@@ -4,7 +4,7 @@ class JmonetteDotfiles < Formula
   homepage 'http://jmonette.github.io/dotfiles'
   url 'https://github.com/jmonette/dotfiles/archive/master.zip'
   version '0.1'
-  sha1 '2b4b3ee75a3ca1221c44f1f87f2a4ca60a1ea391'
+  sha1 'cf491de9d9fe30114a0475d07cbcf88288351270'
 
   def install
     system "./setup.sh"
