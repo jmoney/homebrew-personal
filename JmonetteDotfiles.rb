96eea71c8d115f993d8a9ab4a5da3287c429a1b5
Jonathan Monette <jmonette@homeaway.com>
2013-05-05 18:16:57 -0500

    Added JmonetteDotfiles test brew

---
+++ JmonetteDotfiles.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class JmonetteDotfiles < Formula
+  homepage 'http://jmonette.github.io/dotfiles'
+  url 'https://github.com/jmonette/dotfiles/archive/master.zip'
+  sha1 '2b4b3ee75a3ca1221c44f1f87f2a4ca60a1ea391'
+
+  def install
+    system "./setup.sh"
+  end
+end
