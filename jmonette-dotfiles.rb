f596291bbf3b5822ad04478f5d326c6164598f2b
Jonathan Monette <jon.monette@gmail.com>
2014-10-23 13:12:14 -0500

    fix sha1

--- jmonette-dotfiles.rb
+++ jmonette-dotfiles.rb
@@ -5,7 +5,7 @@ class JmonetteDotfiles < Formula
   homepage 'http://jmonette.github.io/dotfiles'
   url 'https://github.com/jmonette/dotfiles/archive/master.zip'
   version '0.1'
-  sha1 'cf491de9d9fe30114a0475d07cbcf88288351270'
+  sha1 'f96a45f9faa62e272b3054dc5ba4bd33d1bf8052'
 
   depends_on 'coreutils' => :recommended
 
