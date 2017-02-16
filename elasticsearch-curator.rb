a7c910df540168c789d6dd496678ec8defbec8e9
Jonathan Monette <jon.monette@gmail.com>
2017-02-16 12:46:31 -0600

    Use sha256 for elasticsearch-curator

--- elasticsearch-curator.rb
+++ elasticsearch-curator.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ElasticsearchCurator < Formula
   homepage 'https://github.com/elasticsearch/curator/blob/master/README.md'
   url 'https://github.com/elasticsearch/curator/archive/v3.0.3.tar.gz'
-  sha1 'a83a13f01aaa77051b272a43664846727179ab2c'
+  sha256 'a83a13f01aaa77051b272a43664846727179ab2c'
 
   depends_on :python
 
