31200dcbcab77f0cfcb108217d2d2fb9fddce4dc
Jonathan Monette <jon.monette@gmail.com>
2022-06-21 11:08:58 -0500

    Update jmoney-dotfiles.rb

--- jmoney-dotfiles.rb
+++ jmoney-dotfiles.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class JmoneyDotfiles < Formula
   homepage 'https://www.github.com/jmoney/dotfiles'
-  url 'https://github.com/jmoney/dotfiles/archive/v1.2.2.tar.gz'
+  url 'https://github.com/jmoney/dotfiles/archive/v1.2.3.tar.gz'
   sha256 '3aea3cc43ce183438c3049ba9badc9455df32cc6d7e74d8bd0f69f96308b522e'
 
   head 'https://github.com/jmoney/dotfiles.git'
